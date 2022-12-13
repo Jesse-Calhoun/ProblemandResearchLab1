69b105918e58dbe7695bedb9cdc09aa2c8cf7918
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-13 18:22:00 -0500

    I revised the reverse_word() function so that a value would be returned and could be used to complete task 3. The response to the input() function would be reversed and that value would then be held by the variable 'is_palindrome' and is used alongside the 'response' variable in the while loop used to determine whether or whether not the 'response' and the 'is_palindrome' variable are equal to each other.Then the reults are set to be printed, if not a palindrome then prompt the user for a new repsonse to run through the while loop again.

--- problem_solvinng_2.py
+++ problem_solvinng_2.py
@@ -1,16 +1,29 @@
 #task 1 on Problem Solving 2
 # word = input('Type a random word. ')
 # reversed_word = ''
-def reverse_word():
-    word = input('Type a random word. ')
+def reverse_word(word):
+    #word = input('Type a random word. ')
     reversed_word = ''
     for index in range(len(word) -1, -1, -1):
         reversed_word += word[index]
-    print(reversed_word)
-reverse_word()
+    return reversed_word
+#reverse_word()
 #task 2
-def cap_each_first_letter():
-    response = input('Type a random sentence. ')
-    capped_response = response.title()  
-    print(capped_response)  
-cap_each_first_letter()
+# def cap_each_first_letter():
+#     response = input('Type a random sentence. ')
+#     capped_response = response.title()  
+#     print(capped_response)  
+# cap_each_first_letter()
+#task 3
+def run_palindrome():
+    response = input("Type a word you believe to be a Palindrome: ")
+    is_palindrome = reverse_word(response)
+
+    while is_palindrome != response:
+        response = input('This is not a Palindrome. Try again ')
+        is_palindrome = reverse_word(response)
+        if is_palindrome == response:
+            continue
+    print('This is a Palindrome!')
+
+run_palindrome()
