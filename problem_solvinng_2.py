6392a0e2ef6a830b84d4a01b0de5dea1459c7afc
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-14 14:25:23 -0500

    I am revisiting task 2 of lab 2 and attempting without the .title() function. I begin by gathering the response of a user input, to convert into the string I will be capitalizing each first letter to. So far i have used the .split() function to split the string where a space shows up in the string by typning out response.split(' ') and gathering that within a varaible 'results'. I've printed the 'results' variable to make sure the words are splitting where necessary.

--- problem_solvinng_2.py
+++ problem_solvinng_2.py
@@ -1,30 +1,36 @@
 #task 1 on Problem Solving 2
 # word = input('Type a random word. ')
 # reversed_word = ''
-def reverse_word(word):
-    #word = input('Type a random word. ')
-    reversed_word = ''
-    for index in range(len(word) -1, -1, -1):
-        reversed_word += word[index]
-    return reversed_word
+# def reverse_word(word):
+#     #word = input('Type a random word. ')(<---needed for it's own function unless another function is gathering userinput)
+#     reversed_word = ''
+#     for index in range(len(word) -1, -1, -1):
+#         reversed_word += word[index]
+#     return reversed_word
     #print(reversed_word)  <---commented this out so I could return a value rather than print to be used in task 3
 #reverse_word()
 #task 2 
+def cap_each_first_letter():
+    response = input('Type A Sentence To Be Formatted The Same As This Request: ')
+    result = response.split(' ')
+    print(result)
+
+cap_each_first_letter()    
 # def cap_each_first_letter():
 #     response = input('Type a random sentence. ')
 #     capped_response = response.title()  
 #     print(capped_response)  
 # cap_each_first_letter()
 #task 3
-def run_palindrome():
-    response = input("Type a word you believe to be a Palindrome: ")
-    is_palindrome = reverse_word(response)
+# def run_palindrome():
+#     response = input("Type a word you believe to be a Palindrome: ")
+#     is_palindrome = reverse_word(response)
 
-    while is_palindrome != response:
-        response = input('This is not a Palindrome. Try again ')
-        is_palindrome = reverse_word(response)
-        if is_palindrome == response:
-            continue
-    print('This is a Palindrome!')
+#     while is_palindrome != response:
+#         response = input('This is not a Palindrome. Try again ')
+#         is_palindrome = reverse_word(response)
+#         if is_palindrome == response:
+#             continue
+#     print('This is a Palindrome!')
 
-run_palindrome()
+# run_palindrome()
