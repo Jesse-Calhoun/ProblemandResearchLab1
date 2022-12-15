cb9f4d8cf3e8ff7ab1adb390b37712f4a68de548
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-15 18:11:13 -0500

    The code on lines 5,6,7 and 9 were commented out because they weren't right in only printing prime number and only printing the same prime number once only.

--- problem_solving_3.py
+++ problem_solving_3.py
@@ -3,10 +3,11 @@
 #Write a method that prints out all prime numbers between 1 and 100 
 for num in range(2, 100, 1):
     #count = 0
-    for i in range(2, (num//2 + 1)):
-        if (num % i == 0):
-            #count += 1
-            break
-        else:
-            print(num)
-            break
+    #for i in range(2, (num//2 + 1)):
+        #if (num % i == 0):
+    if num
+        #count += 1
+        break
+    else:
+        print(num)
+        break
