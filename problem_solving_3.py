9480eb64562b3600dfcdda5eb66dfc1252968995
Jesse Calhoun <jessecalhoun@Jesses-MacBook-Air.local>
2022-12-15 18:07:28 -0500

    I started task 2 of Lab 3 by creating a for loop that would loop through numbers from 2 to 100 in increments of 1 and began writing an if statement to print the number when determined to be a prime number, I have yet to figure a code out to discern whether any of the numbers within the range a prime or not.

--- problem_solving_3.py
+++ problem_solving_3.py
@@ -1,5 +1,12 @@
 #task 2 
 #A prime number is a number that is only divisible by one and itself.
 #Write a method that prints out all prime numbers between 1 and 100 
-
-
+for num in range(2, 100, 1):
+    #count = 0
+    for i in range(2, (num//2 + 1)):
+        if (num % i == 0):
+            #count += 1
+            break
+        else:
+            print(num)
+            break
